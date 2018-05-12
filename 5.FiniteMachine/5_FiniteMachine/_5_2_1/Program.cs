3ec208d45f42f3976602ae55566d7929dfd12694
Unknown <spayn747@mail.ru>
2018-05-12 11:17:20 +0300

    finite state machine

---
+++ 5.FiniteMachine/5_FiniteMachine/_5_2_1/Program.cs
@@ -0,0 +1,180 @@
+﻿using _5_2_1.Properties;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Resources._1;
+            var expected = bool.Parse(Resources._1a);
+
+            Console.WriteLine(input);
+
+            var result = Solve(input);
+
+            Console.WriteLine(result);
+            Contract.Assert(result == expected);
+        }
+
+        static bool Solve(string input)
+        {
+            var state = States.Initial;
+            using (var chars = input.GetEnumerator())
+            {
+                while (state != States.Accept && state != States.Error)
+                {
+                    if (!chars.MoveNext())
+                    {
+                        if (state == States.FracPart || state == States.IntPart)
+                        {
+                            state = States.Accept;
+                        }
+                        else
+                        {
+                            state = States.Error;
+                        }
+
+                        break;
+                    }
+
+                    if (state == States.Initial)
+                    {
+                        if (chars.Current == ' ')
+                        {
+                            state = States.Initial;
+                        }
+                        else if (char.IsDigit(chars.Current))
+                        {
+                            state = States.IntPart;
+                        }
+                        else
+                        {
+                            state = States.Error;
+                        }
+                    }
+                    else if (state == States.IntPart)
+                    {
+                        if (char.IsDigit(chars.Current))
+                        {
+                            state = States.IntPart;
+                        }
+                        else if (chars.Current == '.')
+                        {
+                            state = States.DecPoint;
+                        }
+                        else
+                        {
+                            state = States.Accept;
+                        }
+                    }
+                    else if (state == States.DecPoint)
+                    {
+                        if (char.IsDigit(chars.Current))
+                        {
+                            state = States.FracPart;
+                        }
+                        else
+                        {
+                            state = States.Error;
+                        }
+                    }
+                    else if (state == States.FracPart)
+                    {
+                        if (char.IsDigit(chars.Current))
+                        {
+                            state = States.FracPart;
+                        }
+                        else
+                        {
+                            state = States.Accept;
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+            }
+            
+            return state == States.Accept;
+        }
+
+        enum States
+        {
+            Initial = 1,
+            Accept,
+            Error,
+            IntPart,
+            DecPoint,
+            FracPart
+        }
+
+        class CustomEnumerator : IDisposable
+        {
+            private IEnumerable<char> _initial;
+            private IEnumerator<char> _enumerator;
+            private bool _hasNext;
+            private char _next;
+            private char _current;
+
+            public CustomEnumerator(IEnumerable<char> initial)
+            {
+                _initial = initial;
+                _enumerator = _initial.GetEnumerator();
+                _hasNext = true;
+                if (_enumerator.MoveNext())
+                {
+                    _next = _enumerator.Current;
+                }
+                else
+                {
+                    _hasNext = false;
+                }
+            }
+            
+            public void Dispose()
+            {
+                if (_enumerator != null)
+                {
+                    _enumerator.Dispose();
+                    _enumerator = null;
+                }
+            }
+
+            public bool HasNext()
+            {
+                return _hasNext;
+            }
+
+            public char Next()
+            {
+                if (!_hasNext)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return _next;
+            }
+
+            public void Move()
+            {
+                if (_enumerator.MoveNext())
+                {
+                    _next = _enumerator.Current;
+                }
+                else
+                {
+                    _hasNext = false;
+                }
+            }
+        }
+    }
+}
---
+++ 5.FiniteMachine/5_FiniteMachine/_5_2_2/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "1.113";
+            var expected = 1.113;
+
+            var (isSuccess, value) = Solve(input);
+
+            Console.WriteLine(input);
+            Console.WriteLine($"isSuccess {isSuccess}, value {value}");
+            Contract.Assert(expected == value);
+        }
+        
+        static (bool isSuccess, double value) Solve(string input)
+        {
+            var state = States.Initial;
+            var value = 0.0;
+            var step = 0.1;
+            using (var chars = input.GetEnumerator())
+            {
+                while (state != States.Accept && state != States.Error)
+                {
+                    if (!chars.MoveNext())
+                    {
+                        if (state == States.FracPart || state == States.IntPart)
+                        {
+                            state = States.Accept;
+                        }
+                        else
+                        {
+                            state = States.Error;
+                        }
+
+                        break;
+                    }
+
+                    var (newState, newValue, newStep) = GoToNewState(state, chars.Current, value, step);
+                    state = newState;
+                    value = newValue;
+                    step = newStep;
+                }
+            }
+
+            return (state == States.Accept, value);
+        }
+
+        private static (States newState, double newValue, double newStep) GoToNewState(States state, char current, double value, double step)
+        {
+            States newState;
+            if (state == States.Initial)
+            {
+                if (current == ' ')
+                {
+                    newState = States.Initial;
+                }
+                else if (char.IsDigit(current))
+                {
+                    newState = States.IntPart;
+                    value = int.Parse(current.ToString());
+                }
+                else
+                {
+                    newState = States.Error;
+                }
+            }
+            else if (state == States.IntPart)
+            {
+                if (char.IsDigit(current))
+                {
+                    newState = States.IntPart;
+                    var digit = int.Parse(current.ToString());
+                    value = value * 10 + digit;
+                }
+                else if (current == '.')
+                {
+                    newState = States.DecPoint;
+                    step = 0.1;
+                }
+                else
+                {
+                    newState = States.Accept;
+                }
+            }
+            else if (state == States.DecPoint)
+            {
+                if (char.IsDigit(current))
+                {
+                    newState = States.FracPart;
+                    var (newValue, newStep) = AddFracPart(value, current, step);
+                    value = newValue;
+                    step = newStep;
+                }
+                else
+                {
+                    newState = States.Error;
+                }
+            }
+            else if (state == States.FracPart)
+            {
+                if (char.IsDigit(current))
+                {
+                    newState = States.FracPart;
+                    var (newValue, newStep) = AddFracPart(value, current, step);
+                    value = newValue;
+                    step = newStep;
+                }
+                else
+                {
+                    newState = States.Accept;
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
+            return (newState, value, step);
+        }
+
+        private static (double value, double step) AddFracPart(double value, char ch, double step)
+        {
+            var digit = int.Parse(ch.ToString());
+            var newValue = value + digit * step;
+            var newStep = step / 10;
+            return (newValue, newStep);
+        }
+
+        enum States
+        {
+            Initial = 1,
+            Accept,
+            Error,
+            IntPart,
+            DecPoint,
+            FracPart
+        }
+
+    }
+}
